c05c7a7d66a0799ff321588227a60fb7725520b4
codeXpawan <shahpawan56@gmail.com>
2025-05-24 23:27:02 +0545

    Completed Upper and Lower Case transformation

--- pre-work-workshops/02-values-and-types/03-to-uppercase/to-uppercase.js
+++ pre-work-workshops/02-values-and-types/03-to-uppercase/to-uppercase.js
@@ -1,1 +1,4 @@
 // YOUR CODE BELOW
+myInfo = "Hello World!, I am Pawan Shah passinonate about coding and lerning new things."
+
+console.log(myInfo.toUpperCase());
--- pre-work-workshops/02-values-and-types/04-to-lowercase/to-lowercase.js
+++ pre-work-workshops/02-values-and-types/04-to-lowercase/to-lowercase.js
@@ -1,1 +1,5 @@
 // YOUR CODE BELOW
+// YOUR CODE BELOW
+myInfo = "HeLLo WorLd!, I am PaWan ShAh passInonate abOut Coding and Lerning new Things."
+
+console.log(myInfo.toLowerCase());
