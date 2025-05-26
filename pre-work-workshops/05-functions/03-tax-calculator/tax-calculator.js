0a41f0ea7cf761ccff62bde66559f5ee7de31ac8
codeXpawan <shahpawan56@gmail.com>
2025-05-26 20:23:36 +0545

    functions completed

--- pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
+++ pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+function taxCalculator(price, state){
+    if( state === 'NY' ){
+        return price + price * 0.04;
+    }
+    else if( state === 'NJ' ){
+        return price + price * 0.06625;
+    }
+    else{
+        error('Invalid state');
+        return null;
+    }
+}
--- pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
+++ pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
@@ -1,1 +1,26 @@
 // YOUR CODE BELOW
+function myMnemonic(firat_arg = "", second_arg = "", third_arg = "", fourth_arg = "") {
+    if (firat_arg === "" && second_arg === "" && third_arg === "" && fourth_arg === "") {
+        return "Please provide at least one argument.";
+    }
+    
+    let mnemonic = "";
+    
+    if (firat_arg !== "") {
+        mnemonic += firat_arg.charAt(0).toUpperCase();
+    }
+    
+    if (second_arg !== "") {
+        mnemonic += second_arg.charAt(0).toUpperCase();
+    }
+    
+    if (third_arg !== "") {
+        mnemonic += third_arg.charAt(0).toUpperCase();
+    }
+    
+    if (fourth_arg !== "") {
+        mnemonic += fourth_arg.charAt(0).toUpperCase();
+    }
+    
+    return mnemonic;
+}
